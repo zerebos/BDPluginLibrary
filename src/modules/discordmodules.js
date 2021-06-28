a0e0b166a63f2cfef1aa6fecf57912e90fa65b5a
Strencher <46447572+Strencher@users.noreply.github.com>
2021-06-28 15:01:05 -0700

    Fix UserPopout in src.

--- src/modules/discordmodules.js
+++ src/modules/discordmodules.js
@@ -210,7 +210,7 @@ export default Utilities.memoizeObject({
     get PopoutOpener() {return WebpackModules.getByProps("openPopout");},
     // Grab with react components
     // get EmojiPicker() {return WebpackModules.getByDisplayName("FluxContainer(EmojiPicker)");},
-    get UserPopout() {return WebpackModules.getByDisplayName("ConnectedUserPopout");},
+    get UserPopout() {return WebpackModules.getModule(m => m.type.displayName === "UserPopoutContainer");},
 
     /* Context Menus */
     get ContextMenuActions() {return WebpackModules.getByProps("openContextMenu");},
