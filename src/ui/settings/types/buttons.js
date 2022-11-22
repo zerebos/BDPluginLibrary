ac46e8ea20af35ab5c6baf427a9fc01a53816eb7
LoneWeeb <73281112+Tharki-God@users.noreply.github.com>
2022-11-23 03:59:03 +0530

    reindent

--- src/ui/settings/types/buttons.js
+++ src/ui/settings/types/buttons.js
@@ -1,50 +1,48 @@
 import SettingField from "../settingfield";
-import {DiscordModules} from "modules";
+import {
+    DiscordModules
+}
+from "modules";
 class ButtonsWrapper extends DiscordModules.React.Component {
-          constructor(props) {
-            super(props);
-          }
-          render() {
-            return DiscordModules.React.createElement(
-              DiscordModules.FlexChild,
-              {
-                grow: 0,
-                direction: DiscordModules.FlexChild.Direction.HORIZONTAL,
-              },
-              ...this.props.buttons.map((data) =>
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return DiscordModules.React.createElement(
+            DiscordModules.FlexChild, {
+            grow: 0,
+            direction: DiscordModules.FlexChild.Direction.HORIZONTAL,
+        },
+            ...this.props.buttons.map((data) =>
                 DiscordModules.React.createElement(
-                  DiscordModules.ButtonData,
-                  {
+                    DiscordModules.ButtonData, {
                     size: DiscordModules.ButtonData.Sizes.LARGE,
                     style: {
-                      marginRight: 20,
+                        marginRight: 20,
                     },
-					disabled: data.disabled,
+                    disabled: data.disabled,
                     onClick: data.onClick,
-                  },
-                  data.text
-                )
-              )
-            );
-          }
-        }
-/** 
+                },
+                    data.text)));
+    }
+}
+/**
  * Creates buttons using discord's built in buttons.
  * @memberof module:Settings
  * @extends module:Settings.SettingField
  */
- class Buttons extends SettingField {
-	 /**
-     * @param {string} name - name label of the setting 
+class Buttons extends SettingField {
+    /**
+     * @param {string} name - name label of the setting
      * @param {string} note - help/note to show underneath or above the setting
      * @param {Array<module:Settings~Button>} buttons - array of all button properties
      */
-          constructor(name, note, buttonsArray) {
-            const NOOP = () => null;
-            super(name, note, NOOP, ButtonsWrapper, {
-              buttons: buttonsArray
-            });
-        }
-        }
+    constructor(name, note, buttonsArray) {
+        const NOOP = () => null;
+        super(name, note, NOOP, ButtonsWrapper, {
+            buttons: buttonsArray
+        });
+    }
+}
 
 export default Buttons;
